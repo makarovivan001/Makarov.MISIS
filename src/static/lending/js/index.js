0ef58465e4681e011bce661d2f42259073fb18ee
Makarov Ivan <makarov.ivan@lit1533.ru>
2025-01-21 22:24:24 +0300

    1)Подкорректировал дизайн

--- src/static/lending/js/index.js
+++ src/static/lending/js/index.js
@@ -62,16 +62,12 @@ function get_club_info(elem) {
         let club_img = document.querySelector('.club-img');
         let club_title = document.querySelector('.club-title');
         let club_desc = document.querySelector('.club-desc');
-        let club_country = document.querySelector('.club-country');
         let club_coach_title = document.querySelector('.club_coach-title');
-        // let club_coach_country = document.querySelector('.club-coach-country');
 
         club_img.src = data.photo;
         club_title.innerText = data.name;
         club_desc.innerText = data.description;
-        club_country.innerText = data.country.name;
         club_coach_title.innerText = `Главный тренер: ${data.coach.surname} ${data.coach.name} ${data.coach.middle_name ?? ''}`;
-        // club_coach_country.innerText = data.coach.country.name;
 
     });
 }
